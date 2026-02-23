08e2e5a1d707050797231951c69f1c09276f1981
alek-niko <email.alek.niko@gmail.com>
2026-02-23 20:30:45 +0100

    chore(js.util.video.helper): initial commit

---
+++ src/js/util/video.helper.js
@@ -0,0 +1,40 @@
+/**
+ * @class VideoHelper
+ * @classdesc Extracts a frame from a video file to use as a preview.
+ */
+class VideoHelper {
+
+	static getThumbnail(file, seekTo = 1) {
+
+		return new Promise((resolve) => {
+
+			const video = document.createElement('video');
+			const canvas = document.createElement('canvas');
+			const ctx = canvas.getContext('2d');
+
+			video.preload = 'metadata';
+			video.src = URL.createObjectURL(file);
+			video.muted = true;
+			video.playsInline = true;
+
+			video.onloadeddata = () => {
+				video.currentTime = seekTo;
+			};
+
+			video.onseeked = () => {
+				canvas.width = video.videoWidth;
+				canvas.height = video.videoHeight;
+				ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+				const dataUrl = canvas.toDataURL('image/jpeg');
+				URL.revokeObjectURL(video.src);
+				resolve(dataUrl);
+			};
+
+			video.onerror = () => resolve(null);
+		});
+
+	}
+	
+}
+
+export default VideoHelper;
