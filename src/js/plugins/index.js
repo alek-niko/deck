94cc233c2f2ff36939fe7640b281d885ea446d2b
alek-niko <email.alek.niko@gmail.com>
2026-03-27 22:03:40 +0100

    chore(js.plugins.index): initial commit

---
+++ src/js/plugins/index.js
@@ -0,0 +1,15 @@
+/**
+ * @module js.plugins.index
+ * @description Entry point for Deck system plugins and services.
+ * 				Plugins are initialized globally via deck.use() during the boot sequence.
+ */
+
+// import Analytics from "./analytics.js";
+
+/**
+ * You can export an Array of plugin classes/instances, 
+ * or an Object keyed by plugin name.
+ */
+export default [
+	// Analytics,
+];
